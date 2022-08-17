f449435a6ae22b5430fe591b10c3c0925fb21e4c
Ramesh Patel <rpatel@oak9.io>
2022-08-17 10:30:44 -0500

    making changes to api-gateway

---
+++ terraform/Users-API/_modules/api-gatewayv2/var.tf
@@ -0,0 +1,18 @@
+variable "lambda_arn_set_user" {
+    type = string
+}
+
+variable "lambda_arn_get_user" {
+    type = string
+}
+
+variable "name" {
+    type = string
+}
+
+variable "protocol_type" {
+    type = string
+}
+variable "description" {
+    type = string
+}
--- terraform/Users-API/main.tf
+++ terraform/Users-API/main.tf
@@ -21,7 +21,7 @@ module "Lambda" {
     depends_on = [module.lambda_s3_bucket]
     source_code_hash_get = data.archive_file.lambda_users_get.output_base64sha256
     source_code_hash_set= data.archive_file.lambda_users_set.output_base64sha256
-    api_source_arn = module.api_gateway.apigatewayv2_api_execution_arn
+    api_source_arn = module.api_gatewayv2.apigatewayv2_api_execution_arn
 }
 module "lambda_s3_bucket" {
     source = "./_modules/s3"
@@ -37,38 +37,47 @@ module "dyanmodb" {
   
 }
 
-#using an external module here to configure api-gateway
-module "api_gateway" {
-  source = "terraform-aws-modules/apigateway-v2/aws"
-  name          = "UserApi-sandbox"
-  description   = "Api-Gateway-UserApi-Testing"
-  protocol_type = "HTTP"
-
-  create = true # to disable all resources
-
-  create_api_gateway               = true  # to control creation of API Gateway
-  create_api_domain_name           = false  # to control creation of API Gateway Domain Name
-  create_default_stage             = true  # to control creation of "$default" stage
-  create_default_stage_api_mapping = true  # to control creation of "$default" stage and API mapping
-  create_routes_and_integrations   = true  # to control creation of routes and integrations
-  create_vpc_link                  = false  # to control creation of VPC link
-
-  # Routes and integrations
-  integrations = {
-    "GET /get-user" = {
-      lambda_arn             = module.Lambda.lambda_arn_UsersGet
-      integration_type = "AWS_PROXY"
-      payload_format_version = "2.0"
-      timeout_milliseconds   = 12000
-    }
-    
-    "POST /set-user" = {
-      integration_type = "AWS_PROXY"
-      payload_format_version = "2.0"
-      lambda_arn             = module.Lambda.lambda_arn_UsersSet
-
-    }
-
-  }
-
+ module "api_gatewayv2" {
+    source = "./_modules/api-gatewayv2"
+    name          = "UserApi-sandbox"
+    protocol_type = "HTTP"
+    description   = "Api-Gateway-UserApi-Testing"
+    lambda_arn_set_user = module.Lambda.lambda_arn_UsersSet
+    lambda_arn_get_user = module.Lambda.lambda_arn_UsersGet
 }
+
+#using an external module here to configure api-gateway
+# module "api_gateway" {
+#   source = "terraform-aws-modules/apigateway-v2/aws"
+#   name          = "UserApi-sandbox"
+#   description   = "Api-Gateway-UserApi-Testing"
+#   protocol_type = "HTTP"
+
+#   create = true # to disable all resources
+
+#   create_api_gateway               = true  # to control creation of API Gateway
+#   create_api_domain_name           = false  # to control creation of API Gateway Domain Name
+#   create_default_stage             = true  # to control creation of "$default" stage
+#   create_default_stage_api_mapping = true  # to control creation of "$default" stage and API mapping
+#   create_routes_and_integrations   = true  # to control creation of routes and integrations
+#   create_vpc_link                  = false  # to control creation of VPC link
+
+#   # Routes and integrations
+#   integrations = {
+#     "GET /get-user" = {
+#       lambda_arn             = module.Lambda.lambda_arn_UsersGet
+#       integration_type = "AWS_PROXY"
+#       payload_format_version = "2.0"
+#       timeout_milliseconds   = 12000
+#     }
+    
+#     "POST /set-user" = {
+#       integration_type = "AWS_PROXY"
+#       payload_format_version = "2.0"
+#       lambda_arn             = module.Lambda.lambda_arn_UsersSet
+
+#     }
+
+#   }
+
+# }
